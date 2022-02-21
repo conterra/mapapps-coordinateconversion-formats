bb6da2cb550cd62dc801051b25f9a0cc81affc9b
s.holtkamp <s.holtkamp@conterra.de>
2022-02-21 13:04:28 +0100

    update to map.apps 4.12.3

--- gulpfile.js
+++ gulpfile.js
@@ -14,25 +14,39 @@
  * limitations under the License.
  */
 const gulp = require("gulp");
-const run_sequence = require('run-sequence');
 const mapapps = require('ct-mapapps-gulp-js');
 
 mapapps.registerTasks({
-    themes: [/*"sample_theme"*/],
-    hasVuetify: true,
+    /* A detailed description of available setting is available at https://www.npmjs.com/package/ct-mapapps-gulp-js */
+    /* a list of themes inside this project */
+    themes: [/*'theme-custom'*/],
+    /* state that the custom theme will be dependant from map.apps everlasting theme that provides the base styles */
     hasBaseThemes: true,
-    forceTranspile: true/*,
-    themeChangeTargets:{
-        "vuetify":[
-            "sample_theme"
+    /* state that we want to support vuetify components and therefore need the the vuetify core styles*/
+    hasVuetify: true,
+    /*themeChangeTargets: {
+        "vuetify": [
+            "theme-custom"
         ]
     }*/
 });
 
-gulp.task("default", function(callback) {
-    run_sequence(
-            "copy-resources",
-            "themes-copy",
-            ["js-transpile", "themes-compile"],
-            callback);
-});
+gulp.task("default",
+    gulp.series(
+        "copy-resources",
+        "themes-copy",
+        gulp.parallel(
+            //"js-lint",
+            //"style-lint",
+            "js-transpile",
+            "themes-compile"
+        )
+    )
+);
+
+gulp.task("compress",
+    gulp.series(
+        "default",
+        "themes-compress"
+    )
+);
